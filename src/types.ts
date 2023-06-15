d88f148cded5bfaf933de6e83b43847bab07a237
Sean Nelson <smnelsonn@gmail.com>
2023-06-14 23:02:17 -0600

    add myalgo transactiont types

--- src/types.ts
+++ src/types.ts
@@ -26,3 +26,28 @@ export type FormattedPeraTransaction = {
   to: string
   type: string
 }
+
+export type FormattedMyAlgoTransaction = {
+  name: string,
+  tag: Uint8Array,
+  type: string,
+  from: {
+      publicKey: Uint8Array,
+      checksum: Uint8Array
+  },
+  to: {
+      publicKey: Uint8Array,
+      checksum: Uint8Array
+  },
+  amount: number,
+  assetIndex: number,
+  note: Uint8Array,
+  flatFee: false,
+  genesisHash: Uint8Array,
+  fee: number,
+  firstRound: number,
+  lastRound: number,
+  genesisID: string,
+  appArgs: [],
+  lease: {}
+}
