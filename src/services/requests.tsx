da31e65228a524b572885cefa82f49d086aa9a22
Sean Nelson <smnelsonn@gmail.com>
2023-06-20 15:25:08 -0600

    more syntax

--- src/services/requests.tsx
+++ src/services/requests.tsx
@@ -9,12 +9,12 @@ export const fetchSuggestedParams = async (): Promise<SuggestedParameters> => {
   const suggestedResponse = await suggestedParamsAxios.get("");
   
   const formattedSuggestedParams = {
-      firstRound: suggestedResponse.data["last-round"],
-      lastRound: suggestedResponse.data["last-round"] + 1000,
-      genesisID: suggestedResponse.data["genesis-id"],
-      genesisHash: suggestedResponse.data["genesis-hash"],
-      flatFee: false,
-      fee: suggestedResponse.data["fee"]
+    firstRound: suggestedResponse.data["last-round"],
+    lastRound: suggestedResponse.data["last-round"] + 1000,
+    genesisID: suggestedResponse.data["genesis-id"],
+    genesisHash: suggestedResponse.data["genesis-hash"],
+    flatFee: false,
+    fee: suggestedResponse.data["fee"]
   };
 
   return formattedSuggestedParams;
